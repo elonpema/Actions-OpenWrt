a35763cafa28aaa4cd37be63548bcff26e4134d1
Albatross <67303241+Albatross-x@users.noreply.github.com>
2023-05-06 20:59:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,13 +8,10 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
