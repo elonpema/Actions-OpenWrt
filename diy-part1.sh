d3308c3f9fcc25babd4d152b53af140d67071c85
elonpema <67303241+elonpema@users.noreply.github.com>
2024-05-22 20:53:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,6 @@
 #=============================================================
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt
