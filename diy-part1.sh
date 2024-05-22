9f23f9ef99bf84c557255f6bbbe730152ff56bf7
elonpema <67303241+elonpema@users.noreply.github.com>
2024-05-22 11:10:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,16 +8,23 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# Remove packages
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/helloworld
+
+# Add a feed source 
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/firkerword/smartdns.git package/smartdns
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot package/luci-app-pushbot
 
-# Lienol/openwrt-package
-# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+git clone https://github.com/leochen-g/wechatBot.git package/wechatBot
+
+# Add packages
+git clone https://github.com/nantayo/My-Pkg package/my-pkg
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
